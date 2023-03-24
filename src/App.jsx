5c09bc0a2df5c6255afe648eeb79b8920cf42aa0
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-03-24 20:40:30 +0700

    use fragment % portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
+
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
-const App = () => {
-  const [userList, setUserList] = useState([]);
+function App() {
+  const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (uName, uAge) => {
-    setUserList((prevUserList) => {
+    setUsersList((prevUsersList) => {
       return [
-        ...prevUserList,
+        ...prevUsersList,
         { name: uName, age: uAge, id: Math.random().toString() },
       ];
     });
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
-      <UsersList users={userList} />
-    </div>
+      <UsersList users={usersList} />
+    </Fragment>
   );
-};
+}
 
 export default App;
